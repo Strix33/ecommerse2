795052705e3502fa1b70490788ec63ceb6c57e63
Strix33 <ghatgerudra81@gmail.com>
2025-08-18 18:19:50 +0530

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,83 @@
+
+import Hero from '../components/Layout/Hero'
+import FeaturedCollection from '../components/Product/FeaturedCollection'
+import FeaturesSection from '../components/Product/FeaturesSection'
+import GenderCollectionCenter from '../components/Product/GenderCollectionCenter'
+import NewArrivals from '../components/Product/NewArrivals'
+import ProductDetails from '../components/Product/ProductDetails'
+import ProductGrid from '../components/Product/ProductGrid'
+
+
+const placeholderProducts=[
+  {
+    _id:1,
+    name:"Product 1",
+    price:100,
+    images :[{url: "https://picsum.photos/500/500?random=3"}],
+  },
+  {
+    _id:2,
+    name:"Product 2",
+    price:100,
+    images :[{url: "https://picsum.photos/500/500?random=4"}],
+  },
+  {
+    _id:3,
+    name:"Product 3",
+    price:100,
+    images :[{url: "https://picsum.photos/500/500?random=5"}],
+  },
+  {
+    _id:4,
+    name:"Product 4",
+    price:100,
+    images :[{url: "https://picsum.photos/500/500?random=6"}],
+  },
+  {
+    _id:5,
+    name:"Product 5",
+    price:100,
+    images :[{url: "https://picsum.photos/500/500?random=7"}],
+  },
+  {
+    _id:6,
+    name:"Product 6",
+    price:100,
+    images :[{url: "https://picsum.photos/500/500?random=8"}],
+  },
+  {
+    _id:7,
+    name:"Product 7",
+    price:100,
+    images :[{url: "https://picsum.photos/500/500?random=9"}],
+  },
+  {
+    _id:8,
+    name:"Product 8",
+    price:100,
+    images :[{url: "https://picsum.photos/500/500?random=10"}],
+  },
+];
+
+const Home = () => {
+  return (
+    <div>
+        <Hero/>
+        <GenderCollectionCenter/>
+        <NewArrivals/>
+        <h2 className='text-3xl text-center font-bold mb-4'>Best Seller</h2>
+        <ProductDetails/>
+
+        <div className="container mx-auto">
+          <h2 className="text-3xl text-center font-bold mb-4">
+            Top Wears for Women
+          </h2>
+          <ProductGrid products={placeholderProducts}/>
+        </div>
+        <FeaturedCollection/>
+        <FeaturesSection/>
+    </div>
+  )
+}
+
+export default Home
